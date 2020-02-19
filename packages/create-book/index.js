abd0c48a3dbdfb30c963444fa9066c92e00f27a4
Cristhian Breunig <cristhianbreunig@gmail.com>
2020-02-19 19:40:55 -0300

    Adicionar conexao com mongodb
    Foi necessario atualizar a invocacao do handler nos testes

--- packages/create-book/index.js
+++ packages/create-book/index.js
@@ -1,6 +1,34 @@
-exports.handler = (event) => {
+const { MongoClient } = require('mongodb');
+
+// Variável de ambiente que guarda o endereço de conexão com o DB
+const MongoURI = process.env.MONGO_URL;
+
+exports.handler = async (event) => {
+  const { name, description, author } = JSON.parse(event.body);
+
+  // Validação dos atributos de entrada da função
+  if (!name || !description || !author) return {
+    statusCode: 400,
+    body: JSON.stringify({ message: 'Insufficient attributes' }),
+  };
+
+  // iniciando conexão com o DB
+  const connection = await MongoClient.connect(MongoURI, { useNewUrlParser: true, poolSize: 10, useUnifiedTopology: true });
+  const db = connection.db('tutorial-api-testavel');
+  const col = db.collection('books');
+
+  try {
+    const response = await col.insertOne({ name, description, author });
+    await connection.close();
     return {
       statusCode: 200,
-      body: JSON.stringify({ event }),
+      body: JSON.stringify(response.ops[0]),
+    };
+  } catch (error) {
+    await connection.close();
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error when inserting book on database' }),
     };
   }
+}
--- packages/create-book/index.test.js
+++ packages/create-book/index.test.js
@@ -1,7 +1,7 @@
 const { handler } = require('./index.js');
 
 describe('create-book', () => {
-  test('Should create a book', () => {
+  test('Should create a book', async () => {
     const payload = {
       body: JSON.stringify({
         name: 'The book of the books',
@@ -9,24 +9,26 @@ describe('create-book', () => {
         author: 'Unknown',
       }),
     };
-    
-    const response = handler(payload);
+ 
+    // await para aguardar resposta da função
+    const response = await handler(payload);
     expect(response).toHaveProperty('statusCode', 200);
     expect(JSON.parse(response.body)).toHaveProperty('name', JSON.parse(payload.body).name);
     expect(JSON.parse(response.body)).toHaveProperty('description', JSON.parse(payload.body).description);
     expect(JSON.parse(response.body)).toHaveProperty('author', JSON.parse(payload.body).author);
     expect(JSON.parse(response.body)).toHaveProperty('_id');
   });
-
-  test('Should fail when create a book', () => {
+  
+  test('Should fail when create a book', async () => {
     const payload = {
       body: JSON.stringify({
         description: 'The great book. You should read.',
         author: 'Unknown',
       }),
     };
-    
-    const response = handler(payload);
+
+    // await para aguardar resposta da função
+    const response = await handler(payload);
     expect(response).toHaveProperty('statusCode', 400);
     expect(JSON.parse(response.body)).toHaveProperty('message', 'Insufficient attributes');
   });
